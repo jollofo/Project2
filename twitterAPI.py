1525111ef325d80c8b30e0d91274a384d0cf5442
teshome-p <108298509+teshome-p@users.noreply.github.com>
2022-07-08 03:37:34 -0400

    Update twitterAPI.py

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,10 +1,9 @@
 import requests
+import pandas
 import json
+import sqlalchemy as db
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAKoiegEAAAAA0IZDCXbvA8NvdTYghlfQ9iHi94U%3DV9Rg7pQVm9zitdrAU5YxRGKyrRbfR58m0bBhrfmj1BK9fvpXdK"
 search_url = "https://api.twitter.com/2/tweets/counts/recent"
-
-# Optional params: start_time,end_time,since_id,until_id,next_token,granularity
-query_params = {'query': '#pokemon -is:retweet','granularity': 'day'}
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
@@ -18,8 +17,20 @@ def connect_to_endpoint(url, params):
     return response.json()
 
 def main():
+    game = input("Enter the name of a game: ")
+    #query_params = {'query': '#' + game + ' -is:retweet','granularity': 'day'}
+    query_params = {'query': game + ' -is:retweet','granularity': 'day'}
     json_response = connect_to_endpoint(search_url, query_params)
-    print(json.dumps(json_response, indent=2, sort_keys=True))
+    #print(json.dumps(json_response, indent=2, sort_keys=True))
+    data = json_response["data"]
+    for d in data:
+      d["game"] = game
+    df = pandas.DataFrame.from_dict(data)
+    engine = db.create_engine('sqlite:///twitterdata.db')
+    df.to_sql('Twitter', con=engine, if_exists='replace', index=False)
+    query_result = engine.execute("SELECT * FROM Twitter;").fetchall()
+    print(pandas.DataFrame(query_result))
+
 
 if __name__ == "__main__":
     main()
